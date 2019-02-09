f2e7b37b816f761eadf3cdad5ef7e886c63f45ae
krzysztoflis <Listodupek6>
2019-02-09 16:39:33 +0100

    Done

--- zad_1.py
+++ zad_1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
-with open('C:/path/numbers.txt') as f:
-    lines = f.read().splitlines()
-print(data)
+data = pd.read_csv('rows.txt', delimiter = '\t')
+
+data = np.subtract(data.max(axis=1),data.min(axis=1))
+print(data.sum())
