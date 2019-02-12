ceadd15f27ddd775ab1494417d1f9cc920d456a7
krzysztoflis <Listodupek6>
2019-02-12 14:48:23 +0100

    Done

--- zad_3.py
+++ zad_3.py
@@ -1,25 +1,20 @@
 import pandas as pd
 import csv
 
-
-
 with open ("zadanie_4_triangle_big.txt", "rt") as file:
-    reader = csv.reader(file, delimiter=' ', skipinitialspace=True)
+    reader = csv.reader(file, delimiter = ' ', skipinitialspace=True)
     data = list()
-    for line in reader:
-        line = filter(None,line)
-        line = list(map(int, line))
-        line.extend([0] * (999 - len(line)))
-        data.append(line)
-
-
+    for row in reader:
+        row = filter(None,row)
+        row = list(map(int, row))
+        row.extend([0] * (999 - len(row)))
+        data.append(row)
 data = pd.DataFrame(data)
 
 
 def Add_V(df, x, z):
     for row in range(x - 1, -1, -1):
         for col in range(row + 1):
-
 
             if (df.iloc[row + 1,col] > df.iloc[row + 1,col + 1]):
                 df.iloc[row,col] += df.iloc[row + 1,col]
@@ -29,10 +24,4 @@ def Add_V(df, x, z):
     return df.iloc[0,0]
 
 print(Add_V(data,999,999))
-
-
-
-# rozmiar maciezy data to (999,2998)
-
-print(data.iloc[1,1])
-print(data.shape)
+#print(data.shape)
